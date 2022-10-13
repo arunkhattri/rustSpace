80732fed5b06cfeb0aecff8a175bf20a7d3add7d
Arun Kr. Khattri <arunkhattri@outlook.com>
2022-10-13 14:49:12 +0530

    variables and mutability...

---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    let y = 7;
+
+    let y = y + 5;
+    println!("The value of y is: {y}");
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("Spaces: {spaces}")
+}
